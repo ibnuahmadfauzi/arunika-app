718ba842eb6e24e1f525adbbf13e931e1b5f9e29
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-03 10:39:45 +0700

    create seed for users

--- config/seedCollections.js
+++ config/seedCollections.js
@@ -46,6 +46,170 @@ const companiesData = [
   },
 ];
 
+const positionsData = [
+  {
+    id: 1,
+    company_id: 1,
+    name: "CEO",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 2,
+    company_id: 1,
+    name: "CMD",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 3,
+    company_id: 1,
+    name: "HCA",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 4,
+    company_id: 1,
+    name: "GA - Asset",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 5,
+    company_id: 1,
+    name: "FTAP",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 6,
+    company_id: 1,
+    name: "MIS - IT",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 7,
+    company_id: 1,
+    name: "IT Staff",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 8,
+    company_id: 1,
+    name: "TSD",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 9,
+    company_id: 1,
+    name: "AP",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 10,
+    company_id: 1,
+    name: "CT",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 11,
+    company_id: 2,
+    name: "Dir Ops KDR",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 12,
+    company_id: 4,
+    name: "Dir Ops CMB",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 13,
+    company_id: 3,
+    name: "Dir Ops ADK",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 14,
+    company_id: 4,
+    name: "Drafter CMB",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 15,
+    company_id: 3,
+    name: "Drafter ADK",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 16,
+    company_id: 2,
+    name: "Field Staff Spv.",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 17,
+    company_id: 2,
+    name: "Field Staff",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 18,
+    company_id: 1,
+    name: "Office Boy",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 19,
+    company_id: 1,
+    name: "Security",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+];
+
+const rulesData = [
+  {
+    id: 1,
+    name: "Admin",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+  {
+    id: 2,
+    name: "User",
+    updated_at: "",
+    created_at: "2025-07-01 10:40:00",
+  },
+];
+
+const usersData = [
+  {
+    id: 1,
+    name: "Ibnu Ahmad Fauzi",
+    email: "ibnu7897@gmail.com",
+    password: "12345678",
+    role_id: "2",
+    position_id: "7",
+    updated_at: "",
+    created_at: "2025-07-01 10:30:45",
+  },
+];
+
 async function seedCompanies() {
   try {
     await client.connect();
@@ -70,8 +234,83 @@ async function seedCompanies() {
   }
 }
 
+async function seedPositions() {
+  try {
+    await client.connect();
+    console.log("✅ Terkoneksi ke MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection("positions");
+
+    // Hapus collection jika sudah ada
+    await collection.deleteMany({});
+
+    // Masukkan data baru
+    const result = await collection.insertMany(positionsData);
+    console.log(
+      `✅ ${result.insertedCount} data berhasil ditambahkan ke koleksi "positions"`
+    );
+  } catch (err) {
+    console.error("❌ Gagal melakukan seeding:", err);
+  } finally {
+    await client.close();
+    console.log("🔌 Koneksi MongoDB ditutup");
+  }
+}
+
+async function seedRules() {
+  try {
+    await client.connect();
+    console.log("✅ Terkoneksi ke MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection("rules");
+
+    // Hapus collection jika sudah ada
+    await collection.deleteMany({});
+
+    // Masukkan data baru
+    const result = await collection.insertMany(rulesData);
+    console.log(
+      `✅ ${result.insertedCount} data berhasil ditambahkan ke koleksi "rules"`
+    );
+  } catch (err) {
+    console.error("❌ Gagal melakukan seeding:", err);
+  } finally {
+    await client.close();
+    console.log("🔌 Koneksi MongoDB ditutup");
+  }
+}
+
+async function seedUsers() {
+  try {
+    await client.connect();
+    console.log("✅ Terkoneksi ke MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection("users");
+
+    // Hapus collection jika sudah ada
+    await collection.deleteMany({});
+
+    // Masukkan data baru
+    const result = await collection.insertMany(usersData);
+    console.log(
+      `✅ ${result.insertedCount} data berhasil ditambahkan ke koleksi "users"`
+    );
+  } catch (err) {
+    console.error("❌ Gagal melakukan seeding:", err);
+  } finally {
+    await client.close();
+    console.log("🔌 Koneksi MongoDB ditutup");
+  }
+}
+
 async function main() {
   await createDatabase();
+  await seedRules();
+  await seedUsers();
+  await seedPositions();
   await seedCompanies();
 }
 
