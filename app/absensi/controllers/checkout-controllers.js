42fdd27c5df6a8f2cd5e39f5d113b413d488f76b
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-08 15:05:52 +0700

    create check out system

--- app/absensi/controllers/checkout-controllers.js
+++ app/absensi/controllers/checkout-controllers.js
@@ -1,57 +1,27 @@
 // import package
-const { MongoClient } = require("mongodb");
-const { ObjectId } = require("mongodb");
+const pool = require("../../../config/db-pg"); // import koneksi database
 require("dotenv").config();
 
-// create url and namedb variable
-const url = process.env.MONGODB_URI;
-const dbName = process.env.DB_ARUNIKACORE;
-
 async function checkOut(req, res) {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-  const client = new MongoClient(url);
+  let attendanceData = req.body;
+  const attendanceId = req.params.id;
 
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    let attendanceData = req.body;
-
-    const attendanceId = req.params.id;
-
-    let myquery = { _id: new ObjectId(attendanceId) };
-    let newvalues = {
-      $set: {
-        check_out_time: `${hours}:${minutes}:${seconds}`,
-        location_out_lat: attendanceData.location_out_lat,
-        location_out_long: attendanceData.location_out_long,
-        new_description: attendanceData.description,
-        new_photo: attendanceData.photo,
-      },
-    };
-
-    const attendances = await db
-      .collection("attendances")
-      .updateOne(myquery, newvalues, function (err, res) {
-        if (err) throw err;
-        console.log("1 document updated");
-        db.close();
-      });
-    res.json(attendances);
+    const result = await pool.query(
+      `
+      UPDATE attendances
+      SET 
+      check_out_time = CURRENT_TIME,
+      location_out_lat = '${attendanceData.location_out_lat}',
+      location_out_long = '${attendanceData.location_out_long}',
+      description_out = '${attendanceData.description_out}',
+      photo_out = '${attendanceData.photo_out}'
+      WHERE id = ${attendanceId};`
+    );
+    res.json(result.rows);
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to parse attendances collections in database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 module.exports = { checkOut };
