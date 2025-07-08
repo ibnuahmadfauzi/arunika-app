d0d3d5d871e6f36d736e2e5aeef8cf42a089ce53
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-08 10:58:23 +0700

    create CRUD positions table with PostgreSQL database

--- app/arunikacore/controllers/positions-controller.js
+++ app/arunikacore/controllers/positions-controller.js
@@ -1,177 +1,72 @@
 // import package
-const { MongoClient } = require("mongodb");
-const { ObjectId } = require("mongodb");
+const pool = require("../../../config/db-pg"); // import koneksi database
 require("dotenv").config();
-
-// create url and namedb variable
-const url = process.env.MONGODB_URI;
-const dbName = process.env.DB_ARUNIKACORE;
-
-// Universal Function
-// get time for updated_at and created_at
-function getCurrentTimestamp() {
-  const now = new Date();
-
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
 
 // get all positions data from database
 async function getAllPositions(req, res) {
-  const client = new MongoClient(url);
-
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    const positions = await db.collection("positions").find({}).toArray();
-    res.json(positions);
+    const result = await pool.query("SELECT * FROM positions");
+    res.json(result.rows);
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to parse positions collections in database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 
 // get specifc position data by id from database
 async function getPositionById(req, res) {
   const id = req.params.id;
-  const client = new MongoClient(url);
-
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    const position = await db
-      .collection("positions")
-      .findOne({ _id: new ObjectId(id) });
-
-    if (!position) {
-      return res.status(404).json({ error: "Position not found" });
-    }
-
-    res.json(position);
+    const result = await pool.query(`SELECT * FROM positions WHERE id=${id}`);
+    res.json(result.rows);
   } catch (err) {
-    console.error("❌ Error saat mengambil data:", err);
-    res.status(500).json({ error: "Failed to get position from database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 
 // store new position data to database
 async function storePosition(req, res) {
-  const client = new MongoClient(url);
+  let positionData = req.body;
 
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    let positionData = req.body;
-    const modifiedPosition = {
-      name: positionData.name || "Unnamed Position",
-      company_id: positionData.company_id || "Empty",
-      updated_at: "",
-      created_at: getCurrentTimestamp(),
-    };
-
-    const positions = await db
-      .collection("positions")
-      .insertOne(modifiedPosition, function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    res.json(positions);
+    const result = await pool.query(
+      `INSERT INTO positions (name, company_id, created_at, updated_at) VALUES ('${positionData.name}', ${positionData.company_id}, CURRENT_TIMESTAMP, NULL);`
+    );
+    res.json(result.rows);
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to parse positions collections in database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 
 // update position data to database by id
 async function updatePosition(req, res) {
-  const client = new MongoClient(url);
+  let positionData = req.body;
+  const positionId = req.params.id;
 
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    let positionData = req.body;
-
-    const positionId = req.params.id;
-
-    let myquery = { _id: new ObjectId(positionId) };
-    let newvalues = {
-      $set: {
-        name: positionData.name || "Unnamed Position",
-        company_id: positionData.company_id || "Empty",
-        updated_at: getCurrentTimestamp(),
-      },
-    };
-
-    const positions = await db
-      .collection("positions")
-      .updateOne(myquery, newvalues, function (err, res) {
-        if (err) throw err;
-        console.log("1 document updated");
-        db.close();
-      });
-    res.json(positions);
+    const result = await pool.query(
+      `UPDATE positions SET name = '${positionData.name}', company_id = ${positionData.company_id}, updated_at=CURRENT_TIMESTAMP WHERE id = '${positionId}';`
+    );
+    res.json(result.rows);
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to parse positions collections in database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 
 // delete position data in database by id
 async function deletePosition(req, res) {
   const positionId = req.params.id;
-  const client = new MongoClient(url);
-
   try {
-    await client.connect();
-    console.log("✅ Berhasil terkoneksi ke MongoDB");
-
-    const db = client.db(dbName);
-    let myquery = { _id: new ObjectId(positionId) };
-    const position = await db
-      .collection("positions")
-      .deleteOne(myquery, function (err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        db.close();
-      });
-
-    if (!position) {
-      return res.status(404).json({ error: "Position not found" });
-    }
-
-    res.json(position);
+    const result = await pool.query(
+      `DELETE FROM positions WHERE id = ${positionId};`
+    );
+    res.json(result.rows);
   } catch (err) {
-    console.error("❌ Error saat mengambil data:", err);
-    res.status(500).json({ error: "Failed to get position from database" });
-  } finally {
-    await client.close();
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
 }
 
