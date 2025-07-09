7a6f3daa6fe8a6a02c9cd3f0439dac403cd5da49
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-09 12:18:59 +0700

    modify origin

--- app/auth/auth.routes.js
+++ app/auth/auth.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./auth.controller");
 const authenticate = require("./auth.middleware");
+router.use(cors());
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
