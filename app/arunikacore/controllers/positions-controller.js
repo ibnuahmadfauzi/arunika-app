26076f9653e75a5430629ee003822ca88714c558
ibnuahmadfauzi <ibnu7897@gmail.com>
2025-07-31 14:40:24 +0700

    modification response and add new comment for documentation in position controller

--- app/arunikacore/controllers/positions-controller.js
+++ app/arunikacore/controllers/positions-controller.js
@@ -1,75 +1,210 @@
-// import package
+// include DB connection
 const pool = require("../../../config/db");
+
+// import dotenv package
 require("dotenv").config();
 
-// get all positions data from database
+// function for get all postions from database
 async function getAllPositions(req, res) {
   try {
-    const result = await pool.query("SELECT * FROM positions");
-    res.json(result.rows);
+    // query for select id and name data from positions table in database
+    const result = await pool.query(`
+    SELECT
+      id, name
+    FROM
+      positions
+    `);
+
+    if (result.rows.length > 0) {
+      // response if result variable not empty
+      res.json({
+        success: true,
+        data: result.rows,
+        message: "Berhasil mengambil semua data posisi",
+      });
+    } else {
+      // response if result variable is empty
+      res.json({
+        success: true,
+        data: null,
+        message: "Tidak ada data posisi yang ditampikan",
+      });
+    }
   } catch (err) {
+    // write error message to terminal
     console.error(err.message);
-    res.status(500).send("Server error");
+
+    // response if process not run or fail
+    res.status(400).json({
+      success: false,
+      message: "Data posisi gagal ditampilkan",
+    });
   }
 }
 
-// get specifc position data by id from database
+// get 1 position data with 'id' parameter
 async function getPositionById(req, res) {
+  // get 'id' from url and contain in id variable
   const id = req.params.id;
   try {
-    const result = await pool.query(`SELECT * FROM positions WHERE id=${id}`);
-    res.json(result.rows);
+    // query for select id, name, company_id from positions table in database
+    // select company name from companies table in database
+    const result = await pool.query(`
+    SELECT
+      positions.id,
+      positions.name,
+      positions.company_id,
+      companies.name AS company_name
+    FROM
+      positions
+    JOIN
+      companies ON positions.company_id = companies.id
+    WHERE
+      positions.id=${id};  
+    `);
+    if (result.rows.length > 0) {
+      // response if position data found
+      res.json({
+        success: true,
+        data: result.rows,
+        message: "Berhasil mengambil data posisi",
+      });
+    } else {
+      // response if position data not found
+      res.json({
+        success: true,
+        data: null,
+        message: "Tidak ada data posisi yang ditampikan",
+      });
+    }
   } catch (err) {
+    // write error message to terminal
     console.error(err.message);
-    res.status(500).send("Server error");
+
+    // response if process not run or fail
+    res.status(400).json({
+      success: false,
+      message: "Data posisi gagal ditampilkan",
+    });
   }
 }
 
-// store new position data to database
+// insert new position data to positions table in database
 async function storePosition(req, res) {
+  // get all data post from front-end
+  // (name and company_id)
   let positionData = req.body;
 
   try {
-    const result = await pool.query(
-      `INSERT INTO positions (name, company_id, created_at, updated_at) VALUES ('${positionData.name}', ${positionData.company_id}, CURRENT_TIMESTAMP, NULL);`
-    );
-    res.json(result.rows);
+    // query for insert data name and company_id to positions table in database
+    const result = await pool.query(`
+      INSERT INTO 
+        positions (
+          name, 
+          company_id, 
+          created_at, 
+          updated_at
+        ) 
+      VALUES 
+        (
+          '${positionData.name}', 
+          ${positionData.company_id}, 
+          CURRENT_TIMESTAMP, 
+          NULL
+        )
+      ;`);
+
+    // response if store position data process is success
+    res.json({
+      success: true,
+      data: null,
+      message: "Data posisi baru berhasil disimpan",
+    });
   } catch (err) {
+    // write error message to terminal
     console.error(err.message);
-    res.status(500).send("Server error");
+
+    // response if insert data to positions table not run or fail
+    res.status(400).json({
+      success: false,
+      message: "Data posisi gagal disimpan",
+    });
   }
 }
 
-// update position data to database by id
+// update position data from positions table in database
 async function updatePosition(req, res) {
+  // get all new data from front-end
+  // (name and company_id)
   let positionData = req.body;
+
+  // get position id from url
   const positionId = req.params.id;
 
   try {
-    const result = await pool.query(
-      `UPDATE positions SET name = '${positionData.name}', company_id = ${positionData.company_id}, updated_at=CURRENT_TIMESTAMP WHERE id = '${positionId}';`
-    );
-    res.json(result.rows);
+    // query for update position data in positions table
+    const result = await pool.query(`
+      UPDATE 
+        positions 
+      SET 
+        name = '${positionData.name}', 
+        company_id = ${positionData.company_id}, 
+        updated_at=CURRENT_TIMESTAMP 
+      WHERE 
+        id = '${positionId}'
+      ;`);
+
+    // response if update position data process is success
+    res.json({
+      success: true,
+      data: null,
+      message: "Data posisi baru berhasil diperbarui",
+    });
   } catch (err) {
+    // write error message to terminal
     console.error(err.message);
-    res.status(500).send("Server error");
+
+    // response if update data to positions table not run or fail
+    res.status(400).json({
+      success: false,
+      message: "Data posisi gagal diperbarui",
+    });
   }
 }
 
-// delete position data in database by id
+// delete position data from positions table in database
 async function deletePosition(req, res) {
+  // get 'id' position from url
   const positionId = req.params.id;
+
   try {
-    const result = await pool.query(
-      `DELETE FROM positions WHERE id = ${positionId};`
-    );
-    res.json(result.rows);
+    // query for delete position data
+    const result = await pool.query(`
+      DELETE FROM 
+        positions 
+      WHERE 
+        id = ${positionId}
+      ;`);
+
+    // response if delete position data process is success
+    res.json({
+      success: true,
+      data: null,
+      message: "Data posisi baru berhasil dihapus",
+    });
   } catch (err) {
+    // write error message to terminal
     console.error(err.message);
-    res.status(500).send("Server error");
+
+    // response if process not run or fail
+    res.status(400).json({
+      success: false,
+      message: "Data posisi gagal dihapus",
+    });
   }
 }
 
+// export all function for use to other file or other location
 module.exports = {
   getAllPositions,
   getPositionById,
